49b27e1c8e326751c22965915ad6239ce20b3f19
faridulhaque <faridulhaquemurshed@gmail.com>
2023-11-09 14:32:28 +0600

    comment done

--- controllers/blogPost.controller.js
+++ controllers/blogPost.controller.js
@@ -17,6 +17,21 @@ const blogPost = async (req, res, next) => {
 }
 
 
+const getCommentatorProfile = async (req, res, next) => {
+    try {
+        const db = getDb()
+        const collection = db.collection("users")
+        const email = req.params.email;
+        const query = { email: email };
+        const result = await collection.findOne(query);
+        res.status(200).json(result)
+
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+
 const getOneBlogPost = async (req, res, next) => {
     try {
         const db = getDb();
@@ -58,6 +73,22 @@ const makeLikeUnlike = async (req, res, next) => {
     res.status(200).json(result);
   };
 
+const saveComment = async (req, res, next) => {
+    const db = getDb();
+    const collection = db.collection("blogPosts");
+    const id = req.body.postId;
+  
+    const email = req.body.userEmail;
+    const comment = req.body.comment;
+  
+    const query = { _id: ObjectId(id) };
+    const doc = await collection.findOne(query)
+    const update = {$push: { comments: {userEmail: email, comment: comment} } }
+
+    const result = await collection.updateOne(query, update);
+    res.status(200).json(result);
+}
+
 const getAllBlogs = async (req, res) => {
     try {
         const db = getDb();
@@ -75,4 +106,4 @@ const getAllBlogs = async (req, res) => {
 }
 
 
-module.exports = { blogPost, getAllBlogs, getOneBlogPost, makeLikeUnlike };
+module.exports = { blogPost, getAllBlogs, getOneBlogPost, makeLikeUnlike, saveComment, getCommentatorProfile };
